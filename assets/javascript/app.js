c92f773ce68a946a394e3b8670e17e33bf271048
Geoff's Macbook <geoffsmacbook@lawn-143-215-118-68.lawn.gatech.edu>
2018-03-24 11:23:17 -0400

    adding javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  // GLOBAL VARIABLES DECLARATIONS
+  let formEmployeeName;
+  let formRole;
+  let formStartDate;
+  let formMonthlyRate;
+
+  // FIREBASE INITIALIZATION
+  let config = {
+    apiKey: "AIzaSyA9jDoGUYomZskVdtfkAwb3z7lm-LMyTNM",
+    authDomain: "timesheetactivity-24b64.firebaseapp.com",
+    databaseURL: "https://timesheetactivity-24b64.firebaseio.com",
+    projectId: "timesheetactivity-24b64",
+    storageBucket: "",
+    messagingSenderId: "722422801080"
+  };
+  firebase.initializeApp(config);
+
+  let database = firebase.database();
+
+  // FIREBASE RETRIEVING DATA
+  database.ref().on("value", function(snapshot) {
+
+    console.log(snapshot);
+    // if (snapshot.child("highBidder").exists() && snapshot.child("highPrice").exists()) {
+
+    // }
+
+// If any errors are experienced, log them to console.
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+
+  // CLICK FUNCTION FOR SUBMITTING WEBFORM
+  $(document).on("click", ".searchButton", function() {
+    formEmployeeName = $("#formEmployeeName").val().trim();
+    formRole = $("#formRole").val().trim();
+    formStartDate = $("#formStartDate").val().trim();
+    formMonthlyRate = $("#formMonthlyRate").val().trim();
+
+    // database.ref().push() {
+    //   name: formEmployeeName,
+    //   role: formRole,
+    // }
+  });
+
+});
